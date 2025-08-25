4e67098298b234a8eea1c9fc332bff87cf79ed01
JoshX <joshx.dev@gmail.com>
2025-08-25 08:57:00 +0100

    update mission

--- app/page.tsx
+++ app/page.tsx
@@ -241,7 +241,7 @@ export default function Home() {
          <div>
           <h3 className="text-2xl font-bold mb-4">Our Mission</h3>
           <p className="text-lg leading-relaxed">
-           Make post abortion care easy to access.
+           Our mission is to make post abortion care easy to access.
           </p>
          </div>
         </div>
