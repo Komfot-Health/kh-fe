596641678eee8cad939bbe7a5b84f442d73e7781
JoshX <joshx.dev@gmail.com>
2025-09-01 09:22:56 +0100

    chore: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { fontSans, fontSerif } from "@/lib/fonts";
 import { siteConfig } from "@/config/site";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
  title: siteConfig.name,
@@ -21,6 +22,19 @@ export default function RootLayout({
     <SiteHeader />
     {children}
     <SiteFooter />
+
+    <Script
+     async
+     src="https://www.googletagmanager.com/gtag/js?id=G-SENQWBX6LW"
+     strategy="afterInteractive"
+    ></Script>
+    <Script strategy="afterInteractive" id="gtm-script">
+     {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SENQWBX6LW');`}
+    </Script>
    </body>
   </html>
  );
