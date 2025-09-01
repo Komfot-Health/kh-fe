9b1280caa2370f484e70592f6ef0d3eedc43dd2c
JoshX <joshx.dev@gmail.com>
2025-09-01 16:01:53 +0100

    update provider copy

--- components/connect-to-provider-link.tsx
+++ components/connect-to-provider-link.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 export const ConnectToProviderLink = ({ children }: { children: ReactNode }) => {
   return  <a
-  href="https://api.whatsapp.com/send?phone=2348109938925&text=Hi"
+  href="https://api.whatsapp.com/send?phone=2348109938925&text=Hi Lade"
   target="_blank"
   rel="noopener noreferrer"
  >
